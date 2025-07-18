7b6a9c55cd67b7384c6680b33820cb929d2c1c93
sukantahirock <haridasnag01715511031@gmail.com>
2025-07-18 12:07:37 +0600

    load if condition

--- fetcher.py
+++ fetcher.py
@@ -1,3 +1,9 @@
 import requests
 url = f"https://api.github.com/repos/sukantahirock/commit_fetcher/commits"
 response = requests.get(url)
+if response.status_code == 200:
+    latest_commit = response.json()[0]
+    sha = latest_commit['sha']
+    message = latest_commit['commit']['message']
+    print(f"Latest Commit Hash: {sha}")
+    print(f"Message: {message}")
